7a040d23d5b50a9dd8464cbc298bfcf4318364c0
Fernanda Matias <fernandasmatias@gmail.com>
2019-03-10 13:20:41 +0000

    Added the Hero Editor: 1. Use og uppercase pipe 2. Use of ngModel for two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+/*  Angular needs to know how the pieces of your application fit together and
+ * what other files and libraries the app requires. This information is called
+ * metadata
+ *  Some of the metadata is in the @Component decorators that you added to 
+ * your component classes. Other critical metadata is in @NgModule decorators.
+ * The most important @NgModule decorator annotates the top-level AppModule 
+ * class.
+ * 
+ *  Every component must be declared in exactly ONE NgModule.
+ */
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../shared/hero';
+
+// @Component is a decorator function that specifies the Angular 
+// metadata for the component.
+@Component({
+  //  the component's CSS element selector
+  selector: 'app-heroes',
+
+  // the location of the component's template file
+  templateUrl: './heroes.component.html',
+
+  // the location of the component's private CSS styles.
+  styleUrls: ['./heroes.component.scss']
+})
+// Always export the component class so you can import it elsewhere ... like in the AppModule.
+export class HeroesComponent implements OnInit {
+
+  public hero: Hero = {
+    id: 1,
+    name: 'Wonder woman'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+    // The ngOnInit is a lifecycle hook.
+    // Angular calls ngOnInit shortly after creating a component. 
+    // It's a good place to put initialization logic.
+
+    // *** ngOnInit() 
+    // Use ngOnInit() for two main reasons:
+    // 1. To perform complex initializations shortly after construction.
+    // 2. To set up the component after Angular sets the input properties.
+    // /!\ Components should be cheap and safe to construct.
+
+    // *** ngOnChages()
+    // Angular calls ngOnChanges() before ngOnInit() and many times after that. It only calls ngOnInit() once.
+    // Angular calls its ngOnChanges() method whenever it detects changes to input properties of the component (or directive)
+
+    // *** ngOnDestroy
+    // Put cleanup logic in ngOnDestroy(), the logic that must run before Angular destroys the directive.
+    // This is the place to free resources that won't be garbage collected automatically. Unsubscribe from Observables and DOM events. 
+    // Stop interval timers. Unregister all callbacks that this directive registered with global or application services. You risk memory 
+    // leaks if you neglect to do so.
+  }
+
+}
+
+
+
